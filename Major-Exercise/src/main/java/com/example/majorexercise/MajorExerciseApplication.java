6e7a4cbada7ac913c178e857bfc2eac179c975e7
75woo <90749331+75woo@users.noreply.github.com>
2023-11-02 23:21:14 +0800

    Add files via upload

---
+++ Major-Exercise/src/main/java/com/example/majorexercise/MajorExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.majorexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorExerciseApplication.class, args);
+    }
+
+}
---
+++ Major-Exercise/src/test/java/com/example/majorexercise/MajorExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.majorexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
